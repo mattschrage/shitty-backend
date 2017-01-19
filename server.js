4bda644cc9be50c19dbe8bd06c863bb5a9c8d2bc
Matt Schrage <matthewschrage@gmail.com>
2017-01-18 23:57:40 -0800

    fixed final res

--- server.js
+++ server.js
@@ -45,14 +45,14 @@ app.get('/feed', function(req, res) {
     res.send('Hello world');
 });
 
-app.get('/db', function (request, response) {
+app.get('/db', function (req, res) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     client.query('SELECT * FROM test_events', function(err, result) {
       done();
       if (err)
        { console.error(err); res.send("Error " + err); }
       else
-       { res.render('pages/db', {results: result.rows} ); }
+       { res.send('pages/db', {results: result.rows} ); }
     });
   });
 });
