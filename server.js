9c7c44f0016b42812c828335df0550c6dc258482
Matt Schrage <matthewschrage@gmail.com>
2017-01-18 23:39:14 -0800

    Database functionality

--- server.js
+++ server.js
@@ -1,18 +1,51 @@
 var express = require('express');
 var app = express();
+app.use(express.json());
+
 var pg = require('pg');
 //var bodyParser = require('body-parser')
 
 //app.use(bodyParser.json())
+//query for get; body for post
+app.get('/event', function(req, res) {
+    var name = req.query.name,
+        boolean = req.query.color;
+
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+      client.query('INSERT INTO items(text, complete) values($1, $2)',[name,boolean], function(err, result) {
+        done();
+        if (err)
+         { console.error(err); response.send("Error " + err); }
+        else
+         {     res.send('Inserted into DB'); }
+      });
+    });
+    // ...
+});
+
+app.get('/init', function(req, res) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('CREATE TABLE test_events(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       {     res.send('Database (test_events) Initialized!'); }
+    });
+  });
+
+});
 
 app.get('/feed', function(req, res) {
 
     res.send('Hello world');
 });
 
+app.post()
+
 app.get('/db', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
+    client.query('SELECT * FROM test_events', function(err, result) {
       done();
       if (err)
        { console.error(err); response.send("Error " + err); }
