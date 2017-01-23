ea4188a813b6a907f74adef2edeb5061785089b9
Matt Schrage <matthewschrage@gmail.com>
2017-01-23 17:21:20 -0500

    save work

--- server.js
+++ server.js
@@ -6,11 +6,70 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 }));
 
+var buildings = require('./buildings.json');
+
 var pg = require('pg');
 //var bodyParser = require('body-parser')
 
 //app.use(bodyParser.json())
 //query for get; body for post
+
+
+var database_url = 'postgres://mws@localhost:5432/peekbackend';
+
+app.post('/loc', function(req, res) {
+
+  var locations = req.body.locations,
+      userId = req.body.userId;
+
+  for (var i = 0; i < locations.length; i++){
+    var data = locations[i];
+    var lat = data.lat,
+        lon = data.lon,
+        location = lat + "," + lon,
+        timestamp = data.timestamp;
+
+        pg.connect(database_url || process.env.DATABASE_URL, function(err, client, done) {
+          client.query('INSERT INTO locations(location, startDate, userId) values($1, $2, $3)',[location, timestamp, userId], function(err, result) {
+            done();
+            if (err)
+             { console.error(err); res.send("Error " + err); }
+            else
+             {     res.send('Inserted location into DB'); }
+          });
+        });
+  }
+
+
+
+});
+
+function hexToRgb(hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
+}
+
+
+function searchBuildings(name) {
+
+
+    for (var i = 0; i < buildings.length; i++) {
+
+      var building = buildings[i];
+      if (building.name && building.name.substring(0, name.length).toLowerCase() === name.toLowerCase()) {
+        console.log(building);
+        return ""+ building.lat+" , " + building.lng;
+      }
+    }
+
+    console.log("NO BUILDING FOUND FOR \""+name+"\"");
+
+}
+
 app.post('/event', function(req, res) {
     var name = req.body.name,
         icon = req.body.icon,
@@ -21,37 +80,32 @@ app.post('/event', function(req, res) {
         locationBuilding = req.body.locationBuilding,
         locationRoom = req.body.locationRoom,
         locationName = locationBuilding + " " + locationRoom;
-        location = req.body.location,
-        color = req.body.color;
+        location = req.body.location;
+        //convert hex to rgb
+        var rgb = hexToRgb(req.body.color);
+        console.log(rgb);
+        var color  = "" + rgb.r / 255 + " " + rgb.g / 255 + " " + rgb.b / 255 + " " + 1.0;
+
+        startDate.setTime( startDate.getTime() + 5*60*1000 );
 
     //do some post processing ei. match up Location Name with actual geopoint
+      location = searchBuildings(locationBuilding);
 
-    // set color depending on icon
-    switch (icon) {
-      case "❤️":
-        color = ""
-        break;
-      case "❤️":
-
-        break;
-      default:
-
-    }
 
     //set geopoint depending on location
-    switch (locationBuilding) {
-      case "❤️":
-        color = ""
-        break;
-      case "❤️":
-
-        break;
-      default:
-
-    }
+    // switch (locationBuilding) {
+    //   case "Thayer":
+    //     location = ""
+    //     break;
+    //   case "Straus":
+    //     location = ""
+    //     break;
+    //   default:
+    //
+    // }
 
     console.log(name, icon, startDate, endDate, details, hostName, locationName, location, color);
-    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    pg.connect(database_url || database_url || database_url || process.env.DATABASE_URL, function(err, client, done) {
       client.query('INSERT INTO events(name, icon, startDate, endDate, details, hostName, locationName, location, color) values($1, $2, $3, $4, $5, $6, $7, $8, $9)',[name, icon, startDate, endDate, details, hostName, locationName, location, color], function(err, result) {
         done();
         if (err)
@@ -64,8 +118,8 @@ app.post('/event', function(req, res) {
 });
 
 app.get('/init', function(req, res) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('CREATE TABLE events ( id SERIAL PRIMARY KEY,icon TEXT, name TEXT, details TEXT, hostName TEXT,locationName TEXT,color TEXT,startDate timestamptz, endDate timestamptz,location POINT)', function(err, result) {
+  pg.connect(database_url || process.env.DATABASE_URL, function(err, client, done) {
+    client.query('CREATE TABLE locations ( id SERIAL PRIMARY KEY, startDate timestamptz, location POINT, userId TEXT)', function(err, result) {
       done();
       if (err)
        { console.error(err); res.send("Error " + err); }
@@ -76,20 +130,29 @@ app.get('/init', function(req, res) {
 
 });
 
+app.get('/hits', function(req, res) {
+  res.send({"count":"1234"});
+});
+
+
 app.get('/feed', function(req, res) {
   console.log("FEED");
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+  pg.connect(database_url || process.env.DATABASE_URL, function(err, client, done) {
 //AND startDate <= (now() + interval \'7 days\')
-    client.query('SELECT * FROM events WHERE startDate >= (now() - interval \'3 hours\');', function(err, result) {
+//WHERE startDate >= (now() - interval \'3 hours\')
+    client.query('SELECT * FROM events WHERE startDate >= (now() - interval \'3 hours\') AND startDate <= (now() + interval \'7 days\');', function(err, result) {
       done();
-      if (err)
-       { console.error(err); res.send("Error " + err); }
-      else
-       { console.log(result["rows"]);
+      if (err) {
+         console.error(err); res.send("Error " + err);
+      }
+
+      else {
+        console.log(result["rows"]);
 
        //iterate to seperate into different sections
        var todayDate = new Date();
-       var tomorrowThreshold = new Date(todayDate.getDate() + 1)
+       var tomorrowThreshold = new Date();
+       tomorrowThreshold.setDate(todayDate.getDate() + 1);
        var todayThreshold = tomorrowThreshold;
        todayThreshold.setHours(4,0,0,0);
        tomorrowThreshold.setHours(24,0,0,0);
@@ -99,8 +162,7 @@ app.get('/feed', function(req, res) {
        var upcoming = [];
        for (var i = 0; i < result["rows"].length; i++ ) {
          var row = result["rows"][i];
-         var date = new Date(JSON.stringify(row["startDate"]));
-         console.log("StartDate:"+row["startDate"]+", Stringify:"+JSON.stringify(row["startDate"]));
+         var date = row["startdate"];
 
          var timestamp = date.getTime();
          console.log("Timestamp:"+timestamp+", Today Threshold:"+todayThreshold.getTime());
@@ -122,8 +184,44 @@ app.get('/feed', function(req, res) {
 });
 
 app.get('/db', function (req, res) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM events WHERE startDate <= now()', function(err, result) {
+  pg.connect(database_url || process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM '+req.query.name, function(err, result) {
+      done();
+      if (err)
+       { console.error(err); res.send("Error " + err); }
+      else
+       { console.log(result["rows"]);
+         res.send(result.rows);}
+    });
+  });
+});
+
+app.get('/drop', function (req, res) {
+
+// if (!(req.password === "matthewschrage")) {
+//   res.send("You don't have permission");
+//
+//   return;
+// }
+
+  pg.connect(database_url || process.env.DATABASE_URL, function(err, client, done) {
+    client.query('TRUNCATE '+req.query.name, function(err, result) {
+      done();
+      if (err)
+       { console.error(err); res.send("Error " + err); }
+      else
+       { console.log(result["rows"]);
+         res.send(result.rows);}
+    });
+  });
+});
+
+app.get('/delete', function (req, res) {
+
+
+
+  pg.connect(database_url || process.env.DATABASE_URL, function(err, client, done) {
+    client.query('DELETE FROM '+req.query.name+' WHERE id = '+req.query.id, function(err, result) {
       done();
       if (err)
        { console.error(err); res.send("Error " + err); }
