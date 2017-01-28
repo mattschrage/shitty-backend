c8d07886cd11968ebaf86ed3093906d3775912d6
Matt Schrage <matthewschrage@gmail.com>
2017-01-28 16:49:43 -0500

    add timezone very where

--- server.js
+++ server.js
@@ -190,9 +190,9 @@ app.get('/now', function(req, res) {
 
 app.get('/feed', function(req, res) {
   console.log("FEED");
-  var today = db.query('SELECT * FROM events WHERE startDate > now()::time - interval \'4 hours\' AND startDate < TIMESTAMP \'tomorrow\'::time + interval \'4 hours\' ORDER BY startDate ASC');
-  var tomorrow = db.query('SELECT * FROM events WHERE startDate >= TIMESTAMP \'tomorrow\'::time + interval \'4 hours\' AND startDate < TIMESTAMP \'tomorrow\'::time + interval \'1 day\' ORDER BY startDate ASC');
-  var upcoming = db.query('SELECT * FROM events WHERE startDate >= TIMESTAMP \'tomorrow\'::time + interval \'1 day\' AND startDate < TIMESTAMP \'tomorrow\'::time + interval \'7 day\' ORDER BY startDate ASC');
+  var today = db.query('SELECT * FROM events WHERE startDate > now() - interval \'4 hours\' AND startDate < TIMESTAMP \'tomorrow\' + interval \'4 hours\' ORDER BY startDate ASC');
+  var tomorrow = db.query('SELECT * FROM events WHERE startDate >= TIMESTAMP \'tomorrow\' + interval \'4 hours\' AND startDate < TIMESTAMP \'tomorrow\' + interval \'1 day\' ORDER BY startDate ASC');
+  var upcoming = db.query('SELECT * FROM events WHERE startDate >= TIMESTAMP \'tomorrow\' + interval \'1 day\' AND startDate < TIMESTAMP \'tomorrow\' + interval \'7 day\' ORDER BY startDate ASC');
   Promise.all([today,tomorrow,upcoming])
   .then(function(sections){
     console.log(sections);
