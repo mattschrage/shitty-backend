3765b2adea71b314c70087b0651ac46c0b15e987
Matt Schrage <matthewschrage@gmail.com>
2017-07-01 17:19:28 -0400

    fix

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser')
@@ -28,70 +27,61 @@ var moment = require('moment');
 //   // check db for reminders
 // }, 1000 * 60);
 // ///
-=======
-let express = require('express');
-let bodyParser = require('body-parser');
-let pgp = require('pg-promise')();
-let Fuse = require('fuse.js');
 
-let buildings = require('./buildings.json');
-let config = require('./config');
+app.post('/loc', function(req, res) {
 
-let db = pgp(config.dbUrl);
-let app = express();
+  var locations = req.body.locations,
+      userId = req.body.userId;
 
-let fuse = new Fuse(buildings, {
-  caseSensitive: false,
-  keys: ['name']
-});
->>>>>>> 5799b42377dca20d1d551572a16fa4261d181808
+  for (var i = 0; i < locations.length; i++){
+    var data = locations[i];
+    var lat = data.lat,
+        lon = data.lon,
+        location = lat + "," + lon,
+        timestamp = data.timestamp;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+        pg.connect(database_url || process.env.DATABASE_URL, function(err, client, done) {
 
-app.post('/loc', (req, res) => {
-  let locations = req.body.locations;
-  let userId = req.body.userId;
-
-  for (let data of locations) {
-    let {lat, lon, timestamp} = data;
-    let location = `${lat},${lon}`;
-
-    let query = 'INSERT INTO locations(location, startDate, userId) values($1, $2, $3)';
-
-    // TODO: Move out of loop and into bulk insert to speed up
-    db.any(query, [location, timestamp, userId])
-      .then(() => {
-        res.send('Inserted location into DB');
-      }).catch(err => {
-        console.error(err);
-        res.send(`Error ${err}`);
-      });
+          client.query('INSERT INTO locations(location, startDate, userId) values($1, $2, $3)',[location, timestamp, userId], function(err, result) {
+            done();
+            if (err)
+             { console.error(err); res.send("Error " + err); }
+            else
+             {     res.send('Inserted location into DB'); }
+          });
+        });
   }
+
+
+
 });
 
-function hexToRgb (hex) {
-  let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-  return result ? {
-    r: parseInt(result[1], 16),
-    g: parseInt(result[2], 16),
-    b: parseInt(result[3], 16)
-  } : null;
+function hexToRgb(hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
 }
 
-function searchBuildings (name) {
-  let building = fuse.search(name)[0];
-  if (building) {
-    console.log(building);
-    return `${building.lat} , ${building.lng}`;
-  } else {
-    console.log(`NO BUILDING FOUND FOR "${name}"`);
-  }
+
+function searchBuildings(name) {
+
+
+    for (var i = 0; i < buildings.length; i++) {
+
+      var building = buildings[i];
+      if (building.name && building.name.substring(0, name.length).toLowerCase() === name.toLowerCase()) {
+        console.log(building);
+        return ""+ building.lat+" , " + building.lng;
+      }
+    }
+
+    console.log("NO BUILDING FOUND FOR \""+name+"\"");
+
 }
 
-<<<<<<< HEAD
 function toTimeZone(time, zone) {
     var format = 'YYYY/MM/DD HH:mm:ss ZZ';
     return moment(time, format).tz(zone).format(format);
@@ -136,8 +126,7 @@ app.get('/event', function(req, res) {
     });
   });
 
-
-}
+});
 
 
 app.post('/event', function(req, res) {
@@ -158,20 +147,9 @@ app.post('/event', function(req, res) {
 
         //startDate = toTimeZone(startDate, "EST");
         console.log(startDate);
-=======
-app.post('/event', (req, res) => {
-  let {name, icon, startDate, endDate, details, hostName, locationBuilding, locationRoom} = req.body;
-  let locationName = `${locationBuilding} ${locationRoom}`;
 
-  // let location = req.body.location;
-  let rgb = hexToRgb(req.body.color);
-  let color = `${rgb.r / 255} ${rgb.g / 255} ${rgb.b / 255} 1.0`;
-  let location = searchBuildings(locationBuilding);
->>>>>>> 5799b42377dca20d1d551572a16fa4261d181808
+        //startDate.setTime( startDate.getTime() + 5*60*1000 );
 
-  console.log({name, icon, startDate, endDate, details, hostName, locationName, location, color});
-
-<<<<<<< HEAD
     //do some post processing ei. match up Location Name with actual geopoint
       //location = searchBuildings(locationBuilding);
       console.log(req.body.latitude,req.body.longitude);
@@ -201,32 +179,25 @@ app.post('/event', (req, res) => {
          {
            res.redirect("http://heypeek.com");
          }    //res.send('Inserted into DB'); }
-=======
-  let query = `INSERT INTO events(name, icon, startDate, endDate, details, hostName, locationName, location, color)
-                  values($1, $2, $3, $4, $5, $6, $7, $8, $9)`;
-  let arrayParams = [name, icon, startDate, endDate, details, hostName, locationName, location, color];
-
-  db.any(query, arrayParams)
-      .then(() => {
-        res.send('Inserted into DB');
-      }).catch(err => {
-        console.error(err);
-        res.send(`Error ${err}`);
->>>>>>> 5799b42377dca20d1d551572a16fa4261d181808
       });
-});
-
-app.get('/init', (req, res) => {
-  db.any('CREATE TABLE locations ( id SERIAL PRIMARY KEY, startDate timestamptz, location POINT, userId TEXT)')
-    .then(result => {
-      res.send('Database (events) Initialized!');
-    }).catch(err => {
-      console.error(err);
-      res.send(`Error ${err}`);
     });
+    // ...
 });
 
-<<<<<<< HEAD
+app.get('/init', function(req, res) {
+
+  pg.connect(process.env.DATABASE_URL || database_url, function(err, client, done) {
+    client.query('CREATE TABLE locations ( id SERIAL PRIMARY KEY, startDate timestamptz, location POINT, userId TEXT)', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); res.send("Error " + err); }
+      else
+       {     res.send('Database (events) Initialized!'); }
+    });
+  });
+
+});
+
 app.get('/hits', function(req, res) {
   res.send({"count":"171"});
 });
@@ -265,18 +236,9 @@ app.get('/now', function(req, res) {
        {     res.send(result); }
     });
   });
-=======
-app.get('/hits', (req, res) => {
-  res.send({count: config.hitCount});
->>>>>>> 5799b42377dca20d1d551572a16fa4261d181808
 });
 
-app.get('/feed', (req, res) => {
-  let today = db.any('SELECT * FROM events WHERE startDate > TIMESTAMP \'today\' AND startDate < TIMESTAMP \'tomorrow\' + interval \'4 hours\'');
-  let tomorrow = db.any('SELECT * FROM events WHERE startDate >= TIMESTAMP \'tomorrow\' AND startDate < TIMESTAMP \'tomorrow\' + interval \'1 day\'');
-  let upcoming = db.any('SELECT * FROM events WHERE startDate >= TIMESTAMP \'tomorrow\' + interval \'1 day\' AND startDate < TIMESTAMP \'tomorrow\' + interval \'7 day\'');
 
-<<<<<<< HEAD
 app.get('/feed', function(req, res) {
   console.log("FEED");
   var today = db.query('SELECT * FROM events WHERE startDate > now() AT TIME ZONE \'EST\' - interval \'3 hours\' AND startDate < TIMESTAMP \'tomorrow\' AT TIME ZONE \'EST\' ORDER BY startDate ASC');
@@ -292,57 +254,109 @@ app.get('/feed', function(req, res) {
   .catch(function(err){
     console.log("err",err);
   });
-=======
-  Promise
-    .all([today, tomorrow, upcoming])
-    .then(sections => {
-      console.log(sections);
->>>>>>> 5799b42377dca20d1d551572a16fa4261d181808
 
-      let payload = {
-        'sectionTitles': ['Today', 'Tomorrow', 'This Week'],
-        'sections': sections
-      };
 
-      res.send(payload);
-    }).catch(err => {
-      console.log('err', err);
-    });
+  // pg.connectAsync(database_url || process.env.DATABASE_URL)
+  //   .then(function(client, done) {
+  //
+  //   client.query('SELECT * FROM events WHERE startDate >= (now() - interval \'3 hours\') AND startDate <= (now() + interval \'7 days\');', function(err, result) {
+  //
+  //     done();
+  //
+  //     if (err) {
+  //        console.error(err);
+  //       res.send("Error " + err);
+  //     }
+  //
+  //     else {
+  //       console.log(result["rows"]);
+  //
+  //      //iterate to seperate into different sections
+  //      var todayDate = new Date();
+  //      var tomorrowThreshold = new Date();
+  //      tomorrowThreshold.setDate(todayDate.getDate() + 1);
+  //      var todayThreshold = tomorrowThreshold;
+  //      todayThreshold.setHours(4,0,0,0);
+  //      tomorrowThreshold.setHours(24,0,0,0);
+  //
+  //      var today = [];
+  //      var tomorrow = [];
+  //      var upcoming = [];
+  //      for (var i = 0; i < result["rows"].length; i++ ) {
+  //        var row = result["rows"][i];
+  //        var date = row["startdate"];
+  //
+  //        var timestamp = date.getTime();
+  //        console.log("Timestamp:"+timestamp+", Today Threshold:"+todayThreshold.getTime());
+  //        if (timestamp <= todayThreshold.getTime()) {
+  //           today.push(row);
+  //        } else if (timestamp <= tomorrowThreshold.getTime()) {
+  //           tomorrow.push(row);
+  //        } else {
+  //           upcoming.push(row);
+  //        }
+  //
+  //      }
+  //
+  //      var payload = {"sectionTitles":["Today","Tomorrow","This Week"],"sections":[today, tomorrow, upcoming]}
+  //        res.send(payload);
+  //      }
+  //   });
+  //});
 });
 
-app.get('/db', (req, res) => {
-  // THIS IS A HORRIBLE SQL VULNERABILITY, BUT I'M SURE YOU KNOW
-  db.any(`SELECT * FROM ${req.query.name}`)
-  .then(result => {
-    console.log(result['rows']);
-    res.send(result.rows);
-  }).catch(err => {
-    console.error(err);
-    res.send(`Error ${err}`);
+app.get('/db', function (req, res) {
+
+  pg.connect( process.env.DATABASE_URL || database_url, function(err, client, done) {
+
+    client.query('SELECT * FROM '+req.query.name, function(err, result) {
+      done();
+      if (err)
+       { console.error(err); res.send("Error " + err); }
+      else
+       { console.log(result["rows"]);
+         res.send(result.rows);}
+    });
   });
 });
 
-app.get('/drop', (req, res) => {
-  // THIS IS A HORRIBLE SQL VULNERABILITY, BUT I'M SURE YOU KNOW
-  db.any(`TRUNCATE ${req.query.name}`)
-    .then(result => {
-      console.log(result['rows']);
-      res.send(result.rows);
-    }).catch(err => {
-      console.error(err);
-      res.send(`Error ${err}`);
+app.get('/drop', function (req, res) {
+
+// if (!(req.password === "matthewschrage")) {
+//   res.send("You don't have permission");
+//
+//   return;
+// }
+
+
+  pg.connect(process.env.DATABASE_URL || database_url, function(err, client, done) {
+
+    client.query('TRUNCATE '+req.query.name, function(err, result) {
+      done();
+      if (err)
+       { console.error(err); res.send("Error " + err); }
+      else
+       { console.log(result["rows"]);
+         res.send(result.rows);}
     });
+  });
 });
 
-app.get('/delete', (req, res) => {
-  db.any(`DELETE FROM ${req.query.name} WHERE id = ${req.query.id}`)
-    .then(result => {
-      console.log(result['rows']);
-      res.send(result.rows);
-    }).catch(err => {
-      console.error(err);
-      res.send(`Error ${err}`);
+app.get('/delete', function (req, res) {
+
+
+
+  pg.connect(process.env.DATABASE_URL || database_url, function(err, client, done) {
+
+    client.query('DELETE FROM '+req.query.name+' WHERE id = '+req.query.id, function(err, result) {
+      done();
+      if (err)
+       { console.error(err); res.send("Error " + err); }
+      else
+       { console.log(result["rows"]);
+         res.send(result.rows);}
     });
+  });
 });
 
 app.get('/analytics_this-is-a-dangerous-hack', function (req, res) {
